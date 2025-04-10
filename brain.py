40686cc5634563deba4b90cdc1886306e1b90490
WenfongWW <87782692+WenfongWW@users.noreply.github.com>
2025-04-09 20:23:57 -0400

    Update brain.py

--- brain.py
+++ brain.py
@@ -6,12 +6,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from nilearn import plotting
 from nilearn.datasets import fetch_atlas_schaefer_2018
-import gdown
-
-# Download data from Google Drive
-file_id = "1TcoGm7Ys0X2AHEg1AQR4OjBf1pFZIUaI"
-output = "processed_train_data.csv"
-gdown.download(f"https://drive.google.com/uc?id={file_id}", output, quiet=False)
 
 # Load data with caching
 @st.cache_data
